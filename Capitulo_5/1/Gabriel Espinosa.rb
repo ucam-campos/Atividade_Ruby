7194c5857843f02f7a497db4761cb51ccd35b325
Gabriel_Espinosa <gabrielespinosajob@gmail.com>
2019-08-21 16:17:32 -0300

    Ref #1 Adiciona a Resolução da Atividade 1 - Gabriel Espinosa

---
+++ Capitulo_5/1/Gabriel Espinosa.rb
@@ -0,0 +1,7 @@
+print 'qual seu nome? '
+nome = gets.chomp
+print 'qual seu nome do meio? '
+meio = gets.chomp
+print 'qual seu sobrenome? '
+sobrenome = gets.chomp
+puts " Total do tamanho do nome: #{nome.size + meio.size + sobrenome.size}"
