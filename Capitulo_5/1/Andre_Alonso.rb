ea07d9208d4c81afbc079e9247a4c4c692d79f7d
andrealonso <andrealonso1@gmail.com>
2019-08-21 11:47:26 -0300

    Ref #2 | Incluídos os Exec. 01 e 02 do cap 5

---
+++ Capitulo_5/1/Andre_Alonso.rb
@@ -0,0 +1,8 @@
+print 'Infor o primeiro nome:'
+pri = gets.to_s.chomp
+print 'Informe sue nome do meio:'
+meio = gets.to_s.chomp
+print 'Informe seu ultimo nome:'
+ult = gets.to_s.chomp
+
+puts "O tamanho so seu nome é #{pri.size + meio.size + ult.size} caracteres."
---
+++ Capitulo_5/2/Andre_Alonso.rb
@@ -0,0 +1,4 @@
+puts "O que você quer?"
+resposta = gets.chomp
+
+puts "O QUE VOCÊ QUER DIZER COM '#{resposta.upcase}'?!? VOCÊ ESTÁ DEMITIDO!!"
