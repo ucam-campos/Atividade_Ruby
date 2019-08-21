b82e768870599fa861f206c20188bec263170131
YukioArie <yukioarie@gmail.com>
2019-08-21 11:21:56 -0300

    #1 | Adiciona resposta Exerscicio 1 Capitulo 5

---
+++ Capitulo_5/1/Yukio.rb
@@ -0,0 +1,8 @@
+puts("Entre com o Primeiro Nome:")
+primeiro_nome = gets.chomp.size
+puts("Entre com o Nome do Meio:")
+nome_meio = gets.chomp.size
+puts("Entre com o Ultimo Nome:")
+ultimo_nome = gets.chomp.size
+
+puts("Soma total do tamanho dos nomes: #{primeiro_nome + nome_meio + ultimo_nome}")
